302371eb48621efcc82eb7fedb45ef1ed0da414e
isaque cruz <isaqueccruz22@gmail.com>
2026-03-23 20:10:30 -0300

    env supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "https://khzlrjtqklotkaruldoz.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoemxyanRxa2xvdGthcnVsZG96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyMjU0ODQsImV4cCI6MjA4OTgwMTQ4NH0.8TPIjepjrlO2ZodMbDAnfaGWRyj-WFFKRtgXfvCFxVM"
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
